1b1a2f19c4c3c630f688af7a35848df4a0299d36
Heinrich Fabros <johnheinrich.fabros@tup.edu.ph>
2022-03-14 21:42:46 +0800

    Update web routes personnel and dashboard

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,27 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::resource("/personnel", "personnelController");
+Route::get("/personnel/restore/{id}", [
+    "uses" => "personnelController@restore",
+    "as" => "personnel.restore",
+]);
+Route::get("/personnel/forceDelete/{id}", [
+    "uses" => "personnelController@forceDelete",
+    "as" => "personnel.forceDelete",
+]);
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
 });
+
+// Route::get("/login", [personnelController::class, "login"]);
+// Route::post("/check", [personnelController::class, "check"])->name("check");
+// Route::get("/welcome", [personnelController::class, "welcome"]);
+// Route::get("/logout", [personnelController::class, "logout"]);
+// Route::get("/personnel/create", [personnelController::class,"create"]);
+
+Route::get('/', function () {
+    return view('home');
+});
+    
