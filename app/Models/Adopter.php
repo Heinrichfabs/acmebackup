754ef3623d295b8e470e1af66c581ebf341fcd99
Heinrich Fabros <johnheinrich.fabros@tup.edu.ph>
2022-03-18 18:26:13 +0800

    Add adopter model

---
+++ app/Models/Adopter.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+class Adopter extends Model
+{
+    public const VALIDATION_RULES = [
+        "first_name" => ["required", "alpha", "min:3"],
+        "last_name" => ["required", "alpha", "min:3"],
+        "phone_number" => ["required", "numeric", "min:6"],
+        "images" => ["required", "image", "mimes:jpg,png,jpeg,gif", "max:5048"],
+    ];
+
+    use HasFactory;
+
+    use SoftDeletes;
+
+    protected $dates = ["deleted_at"];
+
+    protected $table = "adopters";
+
+    protected $fillable = ["first_name", "last_name", "phone_number", "images"];
+
+    protected $primaryKey = "id";
+
+    protected $guarded = ["id"];
+
+    //public function animal()
+    //{
+    //  return $this->belongsTo("\App\Models\Animal", "animals_id");
+    //}
+}
