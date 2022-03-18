5c36c3c2ac5264bf15a18df399687e93dc1220b7
Heinrich Fabros <johnheinrich.fabros@tup.edu.ph>
2022-03-18 18:27:29 +0800

    Update aut defaults fields personnels

--- config/auth.php
+++ config/auth.php
@@ -15,7 +15,7 @@ return [
 
     'defaults' => [
         'guard' => 'web',
-        'passwords' => 'users',
+        'passwords' => 'personnels',
     ],
 
     /*
@@ -38,7 +38,7 @@ return [
     'guards' => [
         'web' => [
             'driver' => 'session',
-            'provider' => 'users',
+            'provider' => 'personnels',
         ],
     ],
 
@@ -60,9 +60,9 @@ return [
     */
 
     'providers' => [
-        'users' => [
+        'personnels' => [
             'driver' => 'eloquent',
-            'model' => App\Models\User::class,
+            'model' => App\Models\Personnel::class,
         ],
 
         // 'users' => [
@@ -87,8 +87,8 @@ return [
     */
 
     'passwords' => [
-        'users' => [
-            'provider' => 'users',
+        'personnels' => [
+            'provider' => 'personnels',
             'table' => 'password_resets',
             'expire' => 60,
             'throttle' => 60,
