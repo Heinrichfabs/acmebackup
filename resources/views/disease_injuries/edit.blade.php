7f9832095acdda8aaab14a0e33019de742c0870e
Heinrich Fabros <johnheinrich.fabros@tup.edu.ph>
2022-03-19 22:19:49 +0800

    Update submit button color

--- resources/views/disease_injuries/edit.blade.php
+++ resources/views/disease_injuries/edit.blade.php
@@ -39,7 +39,7 @@
                     </div>
 
                     <div class="grid grid-cols-2 gap-2 w-full">
-                        <button type="submit" class="bg-green-800 text-white font-bold p-2 mt-5">
+                        <button type="submit" class="bg-green-800 text-black font-bold p-2 mt-5">
                             Submit
                         </button>
                         <a href="{{url()->previous()}}" class="bg-gray-800 text-white font-bold p-2 mt-5 text-center"
