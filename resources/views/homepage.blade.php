4e49863b5c5ae259889fb642b1a6f79fb76e4404
Heinrich Fabros <johnheinrich.fabros@tup.edu.ph>
2022-03-19 22:16:51 +0800

    Update homepage navigation bar

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -1,3 +1,4 @@
+@extends('layouts.app')
 
 <!doctype html>
 <html>
@@ -20,37 +21,11 @@
         <img src="/img/banner.jpg" alt="acmebanner">
     </section>
     <nav class="main-nav">
-            <ul>
-               
-                <li> <a href="{{ URL('login') }}">Login / SignUp</a></li>         
-                <li> <a href="{{ URL('search') }}">search</a></li>     
+            <ul> 
+                <li> <a href="{{ URL('homewelcome') }}">Home</a></li>     
+                <li> <a href="{{ URL('search') }}">Search</a></li>
+                <li> <a href="{{ URL('login') }}">Login</a></li>        
+                <li> <a href="{{ URL('register') }}">Register</a></li>    
             </ul>
     </nav>
     @yield('contents')
-    <main>
-        <article>
-        <h2>What is ACME?</h2>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore asperiores neque perferendis aliquam similique reprehenderit? Repellat consequuntur sapiente facilis recusandae, ut voluptas ab consequatur dolore eligendi iure, dolorem quia soluta?
-         Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aspernatur saepe quia tenetur harum consequatur? Enim voluptatem culpa minus, nemo distinctio optio quaerat exercitationem facere quos et impedit consequatur odit aspernatur?
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptate id, dolor incidunt magnam maiores labore optio blanditiis ut deserunt a quaerat accusamus inventore mollitia eum ullam voluptates delectus animi placeat!</p>
-        </article>
-        <ul class="photos">
-            <li> <img src="/img/photo1.jpg" alt="pet clinic"></li>
-            <li> <img src="/img/photo2.jpg" alt="pet grooming"></li>
-        </ul>
-        </main>
-        <section class="about">
-            <h2>About Us</h2>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore asperiores neque perferendis aliquam similique reprehenderit? Repellat consequuntur sapiente facilis recusandae, ut voluptas ab consequatur dolore eligendi iure, dolorem quia soluta?<br>
-         Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aspernatur saepe quia tenetur harum consequatur? Enim voluptatem culpa minus, nemo distinctio optio quaerat exercitationem facere quos et impedit consequatur odit aspernatur?<br>
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptate id, dolor incidunt magnam maiores labore optio blanditiis ut deserunt a quaerat accusamus inventore mollitia eum ullam voluptates delectus animi placeat!</p>
-        <form>
-            <input type="email" name="email" placeholder="Please type your email">
-        </form>
-        </section>
-        <footer>
-            <p class="copyright">Copyright 2022 ACME</p>
-        </footer>
-    
-</body>
-</html>
